09cbb1e2dbe2de2a63035f34a60fe241bf85545c
Matvey256 <matvey.kiselev.10@bk.ru>
2022-11-17 22:16:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import time
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+bot = telegram.Bot(token=os.getenv("BOT_TOKEN"))
+
+timestamp = None
+
+while True:
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {
+        "Authorization": f"Token {os.getenv('API_TOKEN')}"
+    }
+    payload = {
+        "timestamp": timestamp
+    }
+    try:
+        response = requests.get(
+            url=url,
+            headers=headers,
+            params=payload,
+            timeout=91
+        )
+    except requests.exceptions.ReadTimeout:
+        continue
+    except requests.exceptions.ConnectionError:
+        time.sleep(10)
+        continue
+    response_data = response.json()
+    if response_data["status"] == "timeout":
+        timestamp = response_data["timestamp_to_request"]
+    else:
+        timestamp = response_data["last_attempt_timestamp"]
+        for attempt in response_data["new_attempts"]:
+            if attempt["is_negative"]:
+                result = "К сожалению, в работе нашлись ошибки."
+            else:
+                result = "Предподавателю все понравилось, можно приступатьк следующему уроку!"
+            bot.send_message(
+                text=f'У вас провели работу "{attempt["lesson_title"]}" {attempt["lesson_url"]}\n\n{result}',
+                chat_id=os.getenv("USER_ID")
+            )
+    print(response.json())
