c6bd9aff29dddbac8a5b98b8770d78569c973630
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2023-03-09 20:30:22 +0300

    Update script

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ from dotenv import load_dotenv
 import logging
 
 
+load_dotenv()
+
+bot = telegram.Bot(token=os.getenv("TG_BOT_TOKEN"))
+timestamp = None
+
+tg_handler = TelegramLogsHandler(os.getenv("TG_SERVICE_BOT_TOKEN"), os.getenv("TG_USER_ID"))
+
+logger = logging.getLogger("logger")
+logger.setLevel(logging.WARNING)
+logger.addHandler(tg_handler)
+
+
+
 class TelegramLogsHandler(logging.Handler):
 
     def __init__(self, tg_token, chat_id):
@@ -20,19 +33,8 @@ class TelegramLogsHandler(logging.Handler):
         self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
-    bot = telegram.Bot(token=os.getenv("TG_BOT_TOKEN"))
-
-    timestamp = None
-
-    tg_handler = TelegramLogsHandler(os.getenv("TG_SERVICE_BOT_TOKEN"), os.getenv("TG_USER_ID"))
-
-    logger = logging.getLogger("logger")
-    logger.setLevel(logging.WARNING)
-    logger.addHandler(tg_handler)
-    
+if __name__ == "__main__":  
+    logger.info("Bot was started")
     while True:
         url = "https://dvmn.org/api/long_polling/"
         headers = {
@@ -46,27 +48,29 @@ if __name__ == "__main__":
                 url=url,
                 headers=headers,
                 params=payload,
-                timeout=91
+                timeout=10
             )
             response.raise_for_status()
-        except requests.exceptions.ReadTimeout as error:
-            logger.warning(f"Timeout error: {error}")
-            continue
-        except requests.exceptions.ConnectionError as error:
-            logger.warning(f"Connection error: {error}")
-            time.sleep(10)
-            continue
-        reviews_info = response.json()
-        if reviews_info["status"] == "timeout":
-            timestamp = reviews_info["timestamp_to_request"]
+        except requests.exceptions.HTTPError as error:
+            logger.warning(f'HTTPError: {error}')
+        except requests.exceptions.ReadTimeout:
+            logger.warning('ReadTimeout')
+        except telegram.error.TimedOut:
+            logging.warning("Не удалось отправить сообщение в телеграмм")
+        except requests.exceptions.ConnectionError:
+            logging.warning('Connection Error\nPlease check your internet connection')
         else:
-            timestamp = reviews_info["last_attempt_timestamp"]
-            for attempt in reviews_info["new_attempts"]:
-                if attempt["is_negative"]:
-                    result = "К сожалению, в работе нашлись ошибки."
-                else:
-                    result = "Предподавателю все понравилось, можно приступать следующему уроку!"
-                bot.send_message(
-                    text=f'У вас провели работу "{attempt["lesson_title"]}" {attempt["lesson_url"]}\n\n{result}',
-                    chat_id=os.getenv("TG_USER_ID")
-                )
+            reviews_info = response.json()
+            if reviews_info["status"] == "timeout":
+                timestamp = reviews_info["timestamp_to_request"]
+            else:
+                timestamp = reviews_info["last_attempt_timestamp"]
+                for attempt in reviews_info["new_attempts"]:
+                    if attempt["is_negative"]:
+                        result = "К сожалению, в работе нашлись ошибки."
+                    else:
+                        result = "Предподавателю все понравилось, можно приступать следующему уроку!"
+                    bot.send_message(
+                        text=f'У вас провели работу "{attempt["lesson_title"]}" {attempt["lesson_url"]}\n\n{result}',
+                        chat_id=os.getenv("TG_USER_ID")
+                    )
